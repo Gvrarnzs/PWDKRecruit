a2719e9f0029bc327a92a2217cdb3aa8ffff9c43
Gvrarnzs <arianzas1997@gmail.com>
2022-02-22 18:03:58 +0700

    First Commit

---
+++ pages/Firstpage.js
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+
+export default function FirstPage() {
+    return (
+        <div>
+            <h1>
+                <Link href={"/"}>Back to Home</Link>
+            </h1>
+        </div>
+    )
+}
---
+++ pages/learnpage/pageone.js
@@ -0,0 +1,13 @@
+import Link from "next/link"
+
+
+
+export default function pagelearn() {
+    return (
+        <div>
+            <h1>
+                <Link href={"/"}>Back to Home</Link>
+            </h1>
+        </div>
+    )
+}
