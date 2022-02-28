6978900557cd6029214d02f007c7442ee661cfc5
Gvrarnzs <arianzas1997@gmail.com>
2022-02-28 17:55:24 +0700

    Fixing bug deployment minor

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import Head from 'next/head';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider } from '@emotion/react';
-import theme from './theme';
+import theme from '../components/theme';
 import createEmotionCache from './createEmotionCache';
 import "../styles/globals.css"
 
